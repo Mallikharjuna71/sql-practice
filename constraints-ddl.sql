1914334d84ec7f00452b9f8413b00021b3ac4256
Mallikharjuna71 <meka.mallikharjunareddy@diggibyte.com>
2024-09-30 17:43:54 +0530

    practiced constaints and ddl

--- constraints-ddl.sql
+++ constraints-ddl.sql
@@ -1,5 +1,5 @@
 create table videos(
-	
+
 	id int identity primary key,
 	name nvarchar(30)unique not null,
 	length int not null,
@@ -20,3 +20,20 @@ insert into videos(name, length, zoner, posted)  values('surfing', 10, 'sports',
 select count(*) as cnt from videos group by likes having count(*)>2;
 
 select posted , name from videos where datepart(month, posted) = '8';
+
+exec sp_help 'videos';
+
+update videos set posted = dateadd(day, 3, posted) where datepart(month, posted)='08';
+select * from videos;
+
+
+select datepart(HOUR, posted) from videos where length = 10;
+
+alter table videos alter column  views int default 0;
+select * from videos;
+
+alter table videos drop column likes;
+select * from videos;
+
+alter table videos add share int default 0;
+select * from videos;
